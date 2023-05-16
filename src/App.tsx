dce839c3e0900e6d7562e95d309c7d3e99829201
Eliyan <eliyan.dochev@epitech.eu>
2023-05-16 16:29:13 +0200

    [ADD] Projects base files

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { Button, Container, Grid, Paper, styled } from "@mui/material";
+
+const OutputContainer = styled(`div`)(({ theme }) => ({
+  width: "100%",
+  textAlign: "right",
+  height: "2em",
+  padding: theme.spacing(2),
+  fontSize: "3em",
+  overflow: "hidden",
+}));
+
+const CalculatorBase = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginTop: theme.spacing(4),
+  borderRadius: 15,
+}));
+
+function App() {
+  return (
+    <div></div>
+  )
+}
+
+export default App;
